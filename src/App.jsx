a37ca119905ee4f86b727d98df281c13895290f9
Oxalys <Oxalys@MacBook-Pro-de-Salome.local>
2020-01-24 11:17:56 +0100

    debut portfolio

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import Nav from './Components/Nav';
+import Header from './Components/Header';
+import About from './Components/About';
+import Services from './Components/Services';
+import Portfolio from './Components/Portfolio';
+import Contact from './Components/Contact';
+import Footer from './Components/Footer';
+
+const App = () => {
+
+
+
+  return (
+    <div>
+      <Nav />
+      <Header />
+      <About />
+      <Services />
+      <Portfolio />
+      <Contact />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
