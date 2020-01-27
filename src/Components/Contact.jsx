d8aff339911b9015175ba9c45c0bddcec9f950fa
Oxalys <Oxalys@MacBook-Pro-de-Salome.local>
2020-01-27 16:49:53 +0100

    footer debut

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -4,35 +4,32 @@ const Contact = () => {
 
     return (
         <div id="contact">
-            <h1 className="text-center py-4 display-4 text-light">- Contact -</h1>
-            <form className="d-flex container">
-                <div class="row container justify-content-center">
-                    <div class="form-group col-6">
-                        <label className="h4">Name</label>
-                        <input type="text" class="form-control input-lg" />
-                    </div>
-                    <div class="form-group col-6">
-                        <label className="h4">Email</label>
-                        <input type="text" class="form-control input-lg" />
-                    </div>
-                </div>
-                <div class="row container justify-content-center">
-                    <div class="form-group col-6">
-                        <label className="h4">Phone Number</label>
-                        <input type="text" class="form-control input-lg" />
-                    </div>
-                    <div class="form-group col">
-                        <label className="h4">Sujet</label>
-                        <input type="text" class="form-control input-lg" />
-                    </div>
-                </div>
-                <div className="row container">
-                    <div class="form-group col-12">
-                        <label className="h4">Message</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+            <h2 className="text-center py-4 mb-4 display-4 text-light">- Get in Touch -</h2>
+            <div className="container">
+                <div className="row">
+                    <div className="col-10 mx-auto">
+                        <div className="row">
+                            <div className="col-6 form-group py-3">
+                                <label htmlFor="" className="text-light h4">Name</label>
+                                <input type="text" className="form-control text-light pt-3"/>
+                                </div>
+                            <div className="col-6 form-group py-3">
+                                <label htmlFor="" className="text-light h4">Email</label>
+                                <input type="text" className="form-control text-light pt-3"/>
+                                </div>
+                            <div className="col-12 form-group py-3">
+                                <label htmlFor="" className="text-light h4">Sujet</label>
+                                <input type="text" className="form-control text-light pt-3"/>
+                                </div>
+                            <div className="col-12 form-group py-3">
+                                <label htmlFor="" className="text-light h4">Message</label>
+                                <textarea name="" id="" cols="30" rows="10" className="form-control text-light pt-3"></textarea>
+                            </div>
+                            <div className="col-2 form-group mb-5 mx-auto"><button className="px-4 py-2 rounded text-center text-light h4">Submit</button></div>
+                        </div>
                     </div>
                 </div>
-            </form>
+            </div>
         </div>
     )
 }
