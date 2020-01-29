e0c19338c55b09fe824dd632b833e8723fcc088d
Oxalys <Oxalys@MacBook-Pro-de-Salome.local>
2020-01-29 12:41:25 +0100

    animations suite

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <div id="about" className="container mb-5">
             <h1 className="text-center my-5 display-4">- About me -</h1>
             <div class="row">
-                <div class="col-6"><img src={img} alt="#" /></div>
+                <div class="col-6"><img src={img} alt="#" className="rounded"/></div>
                 <div class="col-5 text-justify mt-3">
                     <h3>FullStack Dev @ MolenGeek</h3>
                     <h5 className="my-5"><p>Email : s.yonte.montero@gmail.com</p>
@@ -19,26 +19,26 @@ const About = () => {
                         <ul class="skill-list">
                             <li class="skill">
                                 <h3>HTML</h3>
-                                <progress class="skill-1" max="100" value="90">
+                                <progress className="skill-1" max="100" value="90">
                                     <strong>Skill Level: 90%</strong>
                                 </progress>
                             </li>
                             <li class="skill">
                                 <h3>JavaScript</h3>
-                                <progress class="skill-2" max="100" value="70">
+                                <progress className="skill-2" max="100" value="70">
                                     <strong>Skill Level: 70%</strong>
                                 </progress>
                             </li>
                             <li class="skill">
                                 <h3>React</h3>
-                                <progress class="skill-3" max="100" value="80">
+                                <progress className="skill-3" max="100" value="80">
                                     <strong>Skill Level: 80%</strong>
                                 </progress>
                             </li>
                         </ul>
                     </div>
                     <div className="d-flex justify-content-center mt-4">
-                        <a href="https://github.com/Oxalys"><input type="button" value="Learn More" className="text-white btn rounded-pill font-weight-bolder p-3 px-4" /></a></div>
+                        <a href="https://github.com/Oxalys"><input type="button" value="Learn More" className="text-white btn  font-weight-bolder p-3 px-4" /></a></div>
                 </div>
             </div>
         </div>
--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -1,8 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const Contact = () => {
 
     // creer un state, stock la maj du click, si click sur input ca devient vrai, vrai va faire un affichage conditionnel sur la class (ternaire)
+
+    const [slide1, setSlide1] = useState("text-light h4");
+    const [slide2, setSlide2] = useState("text-light h4");
+    const [slide3, setSlide3] = useState("text-light h4");
+    const [slide4, setSlide4] = useState("text-light h4");
 
     return (
         <div id="contact">
@@ -12,20 +17,20 @@ const Contact = () => {
                     <div className="col-10 mx-auto">
                         <div className="row">
                             <div className="col-6 form-group py-3">
-                                <label className="text-light h4">Name</label>
-                                <input type="text" className="form-control text-light pt-3"/>
+                                <label className={slide1}>Name</label>
+                                <input type="text" onFocus={() => setSlide1("text-ligth h4 top")} onBlur={() => setSlide1("text-light h4")} className="form-control text-light pt-3"/>
                                 </div>
                             <div className="col-6 form-group py-3">
-                                <label htmlFor="" className="text-light h4">Email</label>
-                                <input type="text" className="form-control text-light pt-3"/>
+                                <label htmlFor="" className={slide2}>Email</label>
+                                <input type="text" onFocus={() => setSlide2("text-ligth h4 top")} onBlur={() => setSlide2("text-light h4")} className="form-control text-light pt-3"/>
                                 </div>
                             <div className="col-12 form-group py-3">
-                                <label htmlFor="" className="text-light h4">Sujet</label>
-                                <input type="text" className="form-control text-light pt-3"/>
+                                <label htmlFor="" className={slide3}>Sujet</label>
+                                <input type="text" onFocus={() => setSlide3("text-ligth h4 top")} onBlur={() => setSlide3("text-light h4")} className="form-control text-light pt-3"/>
                                 </div>
                             <div className="col-12 form-group py-3">
-                                <label htmlFor="" className="text-light h4">Message</label>
-                                <textarea name="" id="" cols="30" rows="10" className="form-control text-light pt-3"></textarea>
+                                <label htmlFor="" className={slide4}>Message</label>
+                                <textarea name="" onFocus={() => setSlide4("text-ligth h4 top")} onBlur={() => setSlide4("text-light h4")} id="" cols="30" rows="10" className="form-control text-light pt-3"></textarea>
                             </div>
                             <div className="col-2 form-group mb-5 mx-auto"><button className="px-4 py-2 rounded text-center text-light h4">Submit</button></div>
                         </div>
