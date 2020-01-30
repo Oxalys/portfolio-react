fbb6d364cc6941e58d7a223aa5ff011f5c893ec5
Oxalys <Oxalys@MacBook-Pro-de-Salome.local>
2020-01-30 14:51:15 +0100

    fini3

--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import './css/style.css';
 import 'jquery/dist/jquery.js';
 import 'popper.js/dist/popper.js';
 import 'bootstrap/dist/js/bootstrap.js';
-import '../node_modules/@fortawesome/fontawesome-free/css/all.min.css';
+import '@fortawesome/fontawesome-free/css/all.min.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
